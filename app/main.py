67f29b64f060598911bd73c4497559a9d76b3b4e
Tania Kolesnik <kolesnik@gmail.com>
2025-10-22 20:32:07 +0100

    add solution 3

--- app/main.py
+++ app/main.py
@@ -7,6 +7,6 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
                 report[operation_type] = 0
             report[operation_type] += int(amount)
 
-    with open(report_file_name, "a") as report_file:
+    with open(report_file_name, "w") as report_file:
         for operation_type, amount in report.items():
             report_file.write(f"{operation_type},{amount}\n")
