dbaae25eba0c7ec8089be21cb450541f02dc3e15
Tania Kolesnik <kolesnik@gmail.com>
2025-10-22 20:17:04 +0100

    add solutuin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with (open(data_file_name, "r") as data_file,
+          open(report_file_name, "a") as report_file):
+        for line in data_file:
+            operation_type, amount = line.split(",")
+            report_file.write(f"{operation_type},{amount}\n")
