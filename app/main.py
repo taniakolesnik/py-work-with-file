3a6f4e8bc68a39fa7608434dba83063c4f617903
Tania Kolesnik <kolesnik@gmail.com>
2025-10-22 20:36:20 +0100

    add solution 4

--- app/main.py
+++ app/main.py
@@ -10,3 +10,4 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     with open(report_file_name, "w") as report_file:
         for operation_type, amount in report.items():
             report_file.write(f"{operation_type},{amount}\n")
+        report_file.write(f"result,{report["supply"] - report["buy"]}\n")
