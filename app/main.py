ceeab881b7f1601c0c880dea3d13b35e35cd7d3d
Tania Kolesnik <kolesnik@gmail.com>
2025-10-22 20:26:28 +0100

    add solution 2a

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     report = {}
-    with open(data_file_name, "r") as data_file, open(report_file_name, "a") as report_file:
+    with open(data_file_name, "r") as data_file:
         for line in data_file:
             operation_type, amount = line.split(",")
             if operation_type not in report:
@@ -10,4 +10,3 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     with open(report_file_name, "a") as report_file:
         for operation_type, amount in report.items():
             report_file.write(f"{operation_type},{amount}\n")
-
